0342e54af71fd585ff3bec6947721f5dba2c474f
LiJiaXin <736730789@qq.com>
2018-03-26 17:44:32 +0800

    controller 多了两个测试方法

--- src/main/java/com/mine/ssm/controller/UserController.java
+++ src/main/java/com/mine/ssm/controller/UserController.java
@@ -5,6 +5,7 @@ import com.mine.ssm.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,9 +24,23 @@ public class UserController {
     private IUserService userService;
 
     @RequestMapping(value = "/showAllUser",method = RequestMethod.GET)
-    public String toShowAllUser(HttpServletRequest request, Model model){
+    public String showAllUser(HttpServletRequest request, Model model){
         int userId = Integer.parseInt(request.getParameter("id"));
         User user = userService.getUserById(userId);
+        model.addAttribute("user",user);
+        return "showUser";
+    }
+
+    @RequestMapping(value = "/toShowAllUser",method = RequestMethod.GET)
+    public String toShowAllUser(@RequestParam("id") Integer id, Model model){
+        User user = userService.getUserById(id);
+        model.addAttribute("user",user);
+        return "showUser";
+    }
+
+    @RequestMapping(value = "/toShowUser/{id}",method = RequestMethod.GET)
+    public String toShowUser(@PathVariable("id") Integer id, Model model){
+        User user = userService.getUserById(id);
         model.addAttribute("user",user);
         return "showUser";
     }
