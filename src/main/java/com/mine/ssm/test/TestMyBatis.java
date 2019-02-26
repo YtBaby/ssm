77aa8fea213a3a1f8328405721bc6cd2ee8e0521
YtBaby <31440045+YtBaby@users.noreply.github.com>
2019-02-26 16:28:11 +0800

    test ssm demo

--- src/main/java/com/mine/ssm/test/TestMyBatis.java
+++ src/main/java/com/mine/ssm/test/TestMyBatis.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * Created by Ljx on 2018/2/28.
+ * Created by Ljx on 2018/2/28 .
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
