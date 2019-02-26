7b6761c3e6534f0aa8dda1cb3ee0dc642430a42f
YtBaby <31440045+YtBaby@users.noreply.github.com>
2019-02-26 16:27:12 +0800

    test ssm demo

--- src/main/java/com/mine/ssm/controller/UserController.java
+++ src/main/java/com/mine/ssm/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Created by LiJiaXin on 2018/3/26.
+ * Created by Ljx on 2018/3/26.
  */
 @Controller
 @RequestMapping("user")
